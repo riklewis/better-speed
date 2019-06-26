2cbd6db97dfba6e19d970d9c25e8b7e95238a7c5
Rik Lewis <rik@rik.onl>
2019-06-26 22:47:42 +0100

    Added option: XML-RPC + Pingbacks

--- svn/trunk/better-speed.php
+++ svn/trunk/better-speed.php
@@ -32,10 +32,9 @@ function better_speed_log($message) {
 
 function better_speed_init() {
 	global $wp;
-	$settings = get_option('better-speed-settings');
 
 	//Emojis
-	if(isset($settings['better-speed-features-emojis']) && $settings['better-speed-features-emojis']==="YES") {
+	if(better_speed_check_setting('emojis')) {
 		remove_action('wp_head', 'print_emoji_detection_script', 7);
 		remove_action('admin_print_scripts', 'print_emoji_detection_script');
 		remove_action('wp_print_styles', 'print_emoji_styles');
@@ -43,11 +42,13 @@ function better_speed_init() {
 		remove_filter('the_content_feed', 'wp_staticize_emoji');
 		remove_filter('comment_text_rss', 'wp_staticize_emoji');
 		remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-		add_filter('tiny_mce_plugins', 'better_speed_tiny_mce_plugins_emojis');
+		add_filter('tiny_mce_plugins', function($plugins) {
+			return array_diff($plugins, array('wpemoji'));
+		});
 	}
 
 	//Embed
-	if(isset($settings['better-speed-features-embed']) && $settings['better-speed-features-embed']==="YES") {
+	if(better_speed_check_setting('embed')) {
 		$wp->public_query_vars = array_diff($wp->public_query_vars, array('embed'));
 		remove_action('rest_api_init', 'wp_oembed_register_route');
 		remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
@@ -55,17 +56,24 @@ function better_speed_init() {
 		remove_action('wp_head', 'wp_oembed_add_host_js');
 		remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
 		add_filter('embed_oembed_discover', '__return_false' );
-		add_filter('tiny_mce_plugins', 'better_speed_tiny_mce_plugins_embed');
-		add_filter('rewrite_rules_array', 'better_speed_rewrite_rules_array_embed');
+		add_filter('tiny_mce_plugins', function($plugins) {
+			return array_diff($plugins, array('wpembed'));
+		});
+		add_filter('rewrite_rules_array', function($rules) {
+			foreach($rules as $rule => $rewrite) {
+				if(strpos($rewrite, 'embed=true')!==false) {
+					unset($rules[$rule]);
+				}
+			}
+			return $rules;
+		});
 	}
 }
 add_action('init', 'better_speed_init');
 
 function better_speed_wp_default_scripts($scripts) {
-	$settings = get_option('better-speed-settings');
-
 	//jQuery Migrate
-	if(isset($settings['better-speed-features-migrate']) && $settings['better-speed-features-migrate']==="YES") {
+	if(better_speed_check_setting('migrate')) {
 		$script = $scripts->registered['jquery'];
 		if($script->deps) {
 			$script->deps = array_diff($script->deps, array('jquery-migrate'));
@@ -74,31 +82,25 @@ function better_speed_wp_default_scripts($scripts) {
 }
 add_filter('wp_default_scripts', 'better_speed_wp_default_scripts');
 
-
-/*
-------------------- Helpers (for actions/filters) -------------------
-*/
-
-function better_speed_tiny_mce_plugins_emojis($plugins) {
-	return array_diff($plugins, array('wpemoji'));
-}
-
-function better_speed_tiny_mce_plugins_embed($plugins) {
-	return array_diff($plugins, array('wpembed'));
-}
-
-function better_speed_rewrite_rules_array_embed($rules) {
-	foreach($rules as $rule => $rewrite) {
-		if(strpos($rewrite, 'embed=true')!==false) {
-			unset($rules[$rule]);
-		}
-	}
-	return $rules;
+//XML-RPC
+if(better_speed_check_setting('xmlrpc')) {
+	add_filter('xmlrpc_enabled', '__return_false');
+	add_filter('pings_open', '__return_false', 9999);
+  add_filter('wp_headers', function($headers) {
+    unset($headers['X-Pingback'], $headers['x-pingback']);
+    return $headers;
+  });
 }
 
 /*
 ----------------------------- Settings ------------------------------
 */
+
+//check checkbox setting
+function better_speed_check_setting($suffix) {
+	$settings = get_option('better-speed-settings');
+  return (isset($settings['better-speed-features-' . $suffix]) && $settings['better-speed-features-' . $suffix]==="YES");
+}
 
 //add settings page
 function better_speed_menus() {
@@ -113,6 +115,7 @@ function better_speed_settings() {
   add_settings_field('better-speed-features-emojis', __('Emojis', 'better-speed-text'), 'better_speed_features_emojis', 'better-speed', 'better-speed-section-features');
   add_settings_field('better-speed-features-embed', __('Embed Objects', 'better-speed-text'), 'better_speed_features_embed', 'better-speed', 'better-speed-section-features');
   add_settings_field('better-speed-features-migrate', __('jQuery Migrate', 'better-speed-text'), 'better_speed_features_migrate', 'better-speed', 'better-speed-section-features');
+  add_settings_field('better-speed-features-xmlrpc', __('XML-RPC + Pingback', 'better-speed-text'), 'better_speed_features_xmlrpc', 'better-speed', 'better-speed-section-features');
 }
 
 //allow the settings to be stored
@@ -120,6 +123,7 @@ add_filter('whitelist_options', function($whitelist_options) {
   $whitelist_options['better-speed'][] = 'better-speed-features-emojis';
   $whitelist_options['better-speed'][] = 'better-speed-features-embed';
   $whitelist_options['better-speed'][] = 'better-speed-features-migrate';
+  $whitelist_options['better-speed'][] = 'better-speed-features-xmlrpc';
   return $whitelist_options;
 });
 
@@ -147,16 +151,15 @@ function better_speed_show_settings() {
   //estimated savings
 	$reqs = 0;
 	$size = 0;
-	$settings = get_option('better-speed-settings');
-	if(isset($settings['better-speed-features-emojis']) && $settings['better-speed-features-emojis']==="YES") {
+	if(better_speed_check_setting('emojis')) {
     $reqs += 1;
 		$size += 16;
 	}
-	if(isset($settings['better-speed-features-embed']) && $settings['better-speed-features-embed']==="YES") {
+	if(better_speed_check_setting('embed')) {
     $reqs += 1;
 		$size += 6;
 	}
-	if(isset($settings['better-speed-features-migrate']) && $settings['better-speed-features-migrate']==="YES") {
+	if(better_speed_check_setting('migrate')) {
 		$reqs += 1;
 		$size += 10;
 	}
@@ -196,30 +199,35 @@ function better_speed_section_features() {
 
 //defined output for settings
 function better_speed_features_emojis() {
-	$settings = get_option('better-speed-settings');
 	$checked = "";
-	if(isset($settings['better-speed-features-emojis']) && $settings['better-speed-features-emojis']==="YES") {
+	if(better_speed_check_setting('emojis')) {
 		$checked = " checked";
 	}
   echo '<label><input id="better-speed-features-emojis" name="better-speed-settings[better-speed-features-emojis]" type="checkbox" value="YES"' . $checked . '> Remove support for emojis in posts <em>(saves at least 1 file request and ~16kb)</em>';
 }
 
 function better_speed_features_embed() {
-	$settings = get_option('better-speed-settings');
 	$checked = "";
-	if(isset($settings['better-speed-features-embed']) && $settings['better-speed-features-embed']==="YES") {
+	if(better_speed_check_setting('embed')) {
 		$checked = " checked";
 	}
   echo '<label><input id="better-speed-features-embed" name="better-speed-settings[better-speed-features-embed]" type="checkbox" value="YES"' . $checked . '> Remove support for embedding objects in posts <em>(saves at least 1 file request and ~6kb)</em>';
 }
 
 function better_speed_features_migrate() {
-	$settings = get_option('better-speed-settings');
 	$checked = "";
-	if(isset($settings['better-speed-features-migrate']) && $settings['better-speed-features-migrate']==="YES") {
+	if(better_speed_check_setting('migrate')) {
 		$checked = " checked";
 	}
   echo '<label><input id="better-speed-features-migrate" name="better-speed-settings[better-speed-features-migrate]" type="checkbox" value="YES"' . $checked . '> Remove support for old jQuery features dropped in 2016 <em>(saves 1 file request and ~10kb)</em>';
+}
+
+function better_speed_features_xmlrpc() {
+	$checked = "";
+	if(better_speed_check_setting('xmlrpc')) {
+		$checked = " checked";
+	}
+  echo '<label><input id="better-speed-features-xmlrpc" name="better-speed-settings[better-speed-features-xmlrpc]" type="checkbox" value="YES"' . $checked . '> Remove support for third-party application access, such as mobile apps';
 }
 
 //add actions
