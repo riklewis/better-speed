2d92d1e44c8dca7f8d07870a6b07f373c8173d6e
Rik Lewis <rik@rik.onl>
2019-07-23 00:06:39 +0100

    Emoji setting should also remove s.w.org DNS prefetch hint

--- svn/trunk/better-speed.php
+++ svn/trunk/better-speed.php
@@ -50,6 +50,7 @@ function better_speed_init() {
 		remove_filter('the_content_feed', 'wp_staticize_emoji');
 		remove_filter('comment_text_rss', 'wp_staticize_emoji');
 		remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('emoji_svg_url', '__return_false');
 		add_filter('tiny_mce_plugins', function($plugins) {
 			return array_diff($plugins, array('wpemoji'));
 		});
